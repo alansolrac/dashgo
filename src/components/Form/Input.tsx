96f50f897466c988eb3bd1780be6245e95107505
Alan Oliveira <developerjs.alan@gmail.com>
2021-05-04 21:36:38 -0300

    add Header and input dashboard

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -13,7 +13,6 @@ export function Input({ name, label, ...rest }: InputProps) {
         <ChakraInput 
         name={name}
         id={name} 
-        type="email" 
         focusBorderColor="pink.500"
         bgColor="gray.900"
         variant="filled"
