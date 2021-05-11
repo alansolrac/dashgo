20df204f9f84b9274979d06ea3c5f34bbc3ec091
Alan Oliveira <developerjs.alan@gmail.com>
2021-05-10 21:04:40 -0300

    add responsive Sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Avatar, Text  } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean,
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return( 
         <Flex align="center">
-        <Box mr="4" TextAlign="right">
-            <Text>Alan Oliveira</Text>        
-            <Text color="gray.300" fontSize="small">
-                developerjs.alan@gmail.com
-            </Text>        
-        </Box>
+        { showProfileData && (
+             <Box mr="4" TextAlign="right">
+             <Text>Alan Oliveira</Text>        
+             <Text color="gray.300" fontSize="small">
+                 developerjs.alan@gmail.com
+             </Text>        
+         </Box>
+        )}
 
         <Avatar size="md" name="Alan Oliveira" src="https://www.github.com/alansolrac.png"/>
     </Flex>
